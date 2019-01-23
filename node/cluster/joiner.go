fdd61a88fa9fd6b45adfaee71d6c6835ea1e22f4
Suchith J N <suchithjn22@gmail.com>
2019-01-23 14:58:22 +0530

    [RAFT] Define Joiner interface and implement a simple file based joiner

---
+++ node/cluster/joiner.go
@@ -0,0 +1,44 @@
+package cluster
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// Joiner specifies how to join the cluster or
+// how node discovery happens in the cluster
+type Joiner interface {
+	// DiscoverNodes tries to discover nodes in the
+	// cluster and if it is successful then it returns
+	// a list containing information of nodes discovered
+	// If there is some error then it is returned
+	DiscoverNodes() ([]NodeInfo, error)
+}
+
+// StaticFileBasedJoiner reads the config file
+// containing information about cluster and parses
+// the list of members from it. Config must be in JSON
+type StaticFileBasedJoiner struct {
+	ClusterConfigFile string
+}
+
+// NewStaticFileBasedJoiner creates a new instance of
+// static file based joiner and returns it. ClusterConfigFile
+// is the path (full or relative) to the JSON config file.
+func NewStaticFileBasedJoiner(clusterConfigFile string) *StaticFileBasedJoiner {
+	return &StaticFileBasedJoiner{clusterConfigFile}
+}
+
+// DiscoverNodes reads the config file provided and gets the
+// list of nodes in the cluster. Any error in the process is returned
+func (joiner *StaticFileBasedJoiner) DiscoverNodes() ([]NodeInfo, error) {
+	configBytes, readErr := ioutil.ReadFile(joiner.ClusterConfigFile)
+	if readErr != nil {
+		return []NodeInfo{}, readErr
+	}
+	var clusterNodes []NodeInfo
+	if unmarshalErr := json.Unmarshal(configBytes, clusterNodes); unmarshalErr != nil {
+		return []NodeInfo{}, unmarshalErr
+	}
+	return clusterNodes, nil
+}
