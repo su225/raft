dd88a5cba211f2a1d7133bd0f1d7b39a80a3bde5
Suchith J N <suchithjn22@gmail.com>
2019-01-24 18:43:55 +0530

    [RAFT] Add Recoverable and Pausable interfaces to support components which can recover state and pause operations

--- node/common/component.go
+++ node/common/component.go
@@ -14,3 +14,24 @@ type ComponentLifecycle interface {
 	// any operation invoked on that component returns error
 	Destroy() error
 }
+
+// Recoverable component specifies that the state of the
+// component is recoverable across crashes.
+type Recoverable interface {
+	// Recover recovers the state of the component
+	// after a restart (happens during start)
+	Recover() error
+}
+
+// Pausable component specifies that the component can temporarily
+// stop accepting commands from the user or other components. If a
+// component is pausable then there must be a way to continue.
+type Pausable interface {
+	// Pause pauses the operation of the component. If the component
+	// is already paused, then this is a no-op. It doesn't affect
+	// destroyed components
+	Pause() error
+	// Resume resumes the operation of the component unless it is
+	// destroyed. If it is already running then this is a no-op
+	Resume() error
+}
