f8dd357b01fbcb7ef1238a288fcb5db3faeaed73
Suchith J N <suchithjn22@gmail.com>
2019-01-27 19:32:20 +0530

    [RAFT] Implement mock state persistence

---
+++ node/state/mock_persist.go
@@ -0,0 +1,41 @@
+package state
+
+import "errors"
+
+// InMemoryRaftStatePersistence represents in-memory persistence.
+// This MUST be used only FOR TESTING PURPOSES.
+type InMemoryRaftStatePersistence struct {
+	ShouldSucceed bool
+	*RaftDurableState
+}
+
+var errStatePersistence = errors.New("state-persistence error")
+
+// NewInMemoryRaftStatePersistence creates a new instance of in-memory
+// raft state persistence. "succeed" indicates whether to fail the operations
+// and "state" is the initial state of the persistence
+func NewInMemoryRaftStatePersistence(succeed bool, state *RaftDurableState) *InMemoryRaftStatePersistence {
+	return &InMemoryRaftStatePersistence{
+		ShouldSucceed:    succeed,
+		RaftDurableState: state,
+	}
+}
+
+// PersistState stores the given state if calls are supposed to be successful.
+// Otherwise they return a generic state persistence-related error
+func (p *InMemoryRaftStatePersistence) PersistState(state *RaftDurableState) error {
+	if !p.ShouldSucceed {
+		return errStatePersistence
+	}
+	p.RaftDurableState = state
+	return nil
+}
+
+// RetrieveState returns the stored state if calls are supposed to be successful.
+// Otherwise they return a generic state persistence-related error
+func (p *InMemoryRaftStatePersistence) RetrieveState() (*RaftDurableState, error) {
+	if !p.ShouldSucceed {
+		return nil, errStatePersistence
+	}
+	return p.RaftDurableState, nil
+}
