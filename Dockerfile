86239666f21e28ee8b791bdbc231debb0467f3e9
Suchith J N <suchithjn22@gmail.com>
2019-02-12 00:00:18 +0530

    [RAFT][DOCKER] Dockerize the raft node

---
+++ Dockerfile
@@ -0,0 +1,59 @@
+# Build the image with go-1.11.1
+FROM golang:1.11.1 AS build
+
+# Copy all the files needed for the build to the container
+RUN mkdir -p /go/src/github.com/su225/raft/
+
+# Build the binary of the raft node with CGO disabled. This
+# will produce a single binary with all libraries linked
+WORKDIR /go/src/github.com/su225/raft
+
+# Enable go modules
+ENV GO111MODULE=on
+
+# Copy the go.mod and go.sum files and then download
+# the dependencies from them. This is because if both of
+# those files do not change then the layer downloading
+# and storing dependencies is also cached
+COPY go.mod .
+COPY go.sum .
+RUN go mod download
+COPY . .
+
+# Finally build the project to produce the binary which will
+# then be moved to another container for the launch
+RUN CGO_ENABLED=0 GOOS=linux \
+    go build -a -installsuffix cgo -ldflags '-extldflags "-static"' -o raft
+
+# Once build is successful copy the binary to another container
+# and run it with appropriate settings
+FROM alpine:3.9
+
+# Create a new user to run the raft node and
+# switch to that user
+RUN mkdir node && \
+    mkdir -p node/cluster-data/log/data && \
+    mkdir -p node/cluster-data/log/metadata && \
+    mkdir -p node/cluster-data/state && \
+    mkdir -p node/cluster-data/snapshot && \
+    mkdir -p node/cluster-data/cluster && \
+    adduser -S -D -H -h /node rnodeuser
+USER rnodeuser
+
+# Define the volumes for this container
+# that must be supplied from the host
+VOLUME node/cluster-data/log
+VOLUME node/cluster-data/state
+VOLUME node/cluster-data/snapshot
+
+# Copy the binary from the build container
+COPY --from=build /go/src/github.com/su225/raft/raft /node
+COPY --from=build /go/src/github.com/su225/raft/scripts/node_start.sh /node
+WORKDIR /node
+
+# Launch the process from the run-script
+CMD sh ./node_start.sh
+
+# Start the node with the settings specified in the container
+# environment and expose appropriate ports for communication
+EXPOSE 6666 7777
---
+++ scripts/node_start.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+echo 'Starting raft node'
+
+DEFAULT_NODE_BIN_PATH=.
+
+DEFAULT_RPC_PORT=6666
+DEFAULT_API_PORT=7777
+
+# Define the directories
+CLUSTER_DIR_ROOT=./cluster-data
+
+DATA_PATH=${CLUSTER_DIR_ROOT}/log/data
+METADATA_PATH=${CLUSTER_DIR_ROOT}/log/metadata/metadata.json
+STATE_PATH=${CLUSTER_DIR_ROOT}/state/state.json
+SNAPSHOT_PATH=${CLUSTER_DIR_ROOT}/snapshot
+
+# Default join mode is set to cluster-file unless
+# it is set to Kubernetes mode in which case it will
+# look for the file containing labels
+DEFAULT_JOIN_MODE='cluster-file'
+
+DEFAULT_ELECTION_TIMEOUT_MILLIS=2000
+HEARTBEAT_INTERVAL_MILLIS=500
+
+RPC_TIMEOUT_MILLIS=1250
+API_TIMEOUT_MILLIS=1500
+API_FWD_TIMEOUT_MILLIS=2000
+
+MAX_CONN_RETRY_ATTEMPTS=3
+
+# Check if node ID is specified. If not, then exit
+if [[ -z "${NODE_ID}" ]]; then
+    echo 'NODE_ID environment variable is not specified'
+    exit 1
+fi
+
+# Check if election timeout is specified. If it is
+# not specified then terminate
+if [[ -z "${ELECTION_TIMEOUT_MILLIS}" ]]; then
+    echo 'ELECTION_TIMEOUT_MILLIS environment variable is not specified'
+    exit 1
+fi
+
+# Common parameters for both modes of cluster formation
+read -r -d '' NODE_ARGS <<EOM
+    --id=${NODE_ID}
+    --api-port=${DEFAULT_API_PORT}
+    --rpc-port=${DEFAULT_RPC_PORT}
+    --log-entry-path=${DATA_PATH} 
+    --log-metadata-path=${METADATA_PATH}
+    --raft-state-path=${STATE_PATH} 
+    --election-timeout=${ELECTION_TIMEOUT_MILLIS}
+    --rpc-timeout=${RPC_TIMEOUT_MILLIS}
+    --api-timeout=${API_TIMEOUT_MILLIS}
+    --api-fwd-timeout=${API_FWD_TIMEOUT_MILLIS}
+    --max-conn-retry-attempts=${MAX_CONN_RETRY_ATTEMPTS}
+    --snapshot-path=${SNAPSHOT_PATH}
+EOM
+
+if [[ -z "${RUNNING_IN_K8S_ENV}" ]]; then
+    CLUSTER_CONFIG_PATH=${CLUSTER_DIR_ROOT}/cluster/config.json
+    echo "Running in local/docker mode. Read config-file in ${CLUSTER_CONFIG_PATH}"
+    ./raft ${NODE_ARGS} \
+        --join-mode=cluster-file \
+        --cluster-config-path=${CLUSTER_CONFIG_PATH}
+else
+    echo 'Currently running in Kubernetes environment. Not supported yet'
+    exit 1
+fi
